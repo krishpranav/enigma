9d0ffe5883ce1126bcec7f22381db814405ebed3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-23 19:37:03 +0530

    enigma: opal[browser]

---
+++ opal/enigma/enigma/browser.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env/ruby
+
+module Enigma
+    module Browser
+        module_function
+
+        Window = JS.global
+        Document = Window.JS[:document]
+        AddEventListener = Window.JS[:addEventListener]
+
+        if Native(Window.JS[:requestAnimationFrame])
+            def animation_frame(&block)
+                Window.JS.requestAnimationFrame(block)
+            end
+        else
+            def animation_frame(&block)
+                block.call
+            end
+        end
+
+        def ready?(&block)
+            AddEventListener.call('load', block)
+        end
+
+        def body
+            Document.JS[:body]
+        end
+
+        def append_child(node, new_node)
+            node = node.to_n unless native?(node)
+            new_node = new_node.to_n unless native?(new_node)
+            node.JS.appendChild(new_node)
+        end
+
+        def query_element(css)
+            Document.JS.querySelector(css)
+        end
+    end
+end
