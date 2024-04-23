2ac5404a405a1320e06164de86c3b47443036d06
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-23 19:27:53 +0530

    enigma: opal[enigma[component(virtual_dom)]]

---
+++ opal/enigma/enigma/component/virtual_dom.rb
@@ -0,0 +1,26 @@
+module Enigma
+    module Component
+      module VirtualDom
+        def component(comp, opts = {})
+          raise Error, "Component is nil in #{self.class} class" if comp.nil?
+          @__virtual_nodes__ ||= []
+          @__virtual_nodes__ << cache_component(comp) do
+            comp = (comp.is_a?(Class) ? comp.new : comp)
+              .with_root_component(@root_component)
+              .inject
+            comp.init
+            comp
+          end.with_props(opts[:props] || {}).render_virtual_dom
+          self
+        end
+  
+        def hook(mthd)
+          VirtualDOM::Hook.method(method(mthd))
+        end
+  
+        def unhook(mthd)
+          VirtualDOM::UnHook.method(method(mthd))
+        end
+      end
+    end
+end
