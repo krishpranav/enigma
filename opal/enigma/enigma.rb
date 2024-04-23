25679ff10ac740722b432f94f2a21c343d8668cd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-23 19:38:42 +0530

    enigma: opal[enigma]

---
+++ opal/enigma/enigma.rb
@@ -0,0 +1,14 @@
+require 'opal'
+require 'js'
+require 'console'
+require 'virtual_dom'
+
+# includes
+require 'enigma/browser'
+require 'enigma/error'
+require 'enigma/injection'
+require 'enigma/component'
+require 'enigma/component/class_methods'
+require 'enigma/component/virtual_dom'
+require 'enigma/component/render'
+require 'enigma/component/cache'
