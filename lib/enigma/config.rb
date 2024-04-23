e3d8d103afba1ecf7f17d3c4ef6753e064ea874e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-23 19:44:06 +0530

    enigma: config

--- lib/enigma/config.rb
+++ lib/enigma/config.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env/ruby
+
+module Enigma
+    module Config
+        STATIC_DIR = '/static'
+        BUILD_DIR = 'dist'
+        APP_DIR = 'app'
+        SERVER_MAIN = 'application'
+        SERVER_INDEX_PATH = 'index.html.erb'
+    end
+end
