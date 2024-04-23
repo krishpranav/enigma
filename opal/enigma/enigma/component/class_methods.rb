9ba1d00cbea09f8c2f55cd509d407676b42a740c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-23 19:23:39 +0530

    opal: enigma[componenet(class_method)] gp

---
+++ opal/enigma/enigma/component/class_methods.rb
@@ -0,0 +1,19 @@
+module Enigma
+    module Component
+        module ClassMethod
+            def mount_to(element)
+                new.mount_to(element)
+            end
+
+            def inject(clazz, opts = {})
+                method_name = opts[:as] || clazz.to_s.downcase
+                @injections || = {}
+                @injections[method_name] = clazz
+            end
+
+            def injections
+                @injections || {}
+            end
+        end
+    end
+end
